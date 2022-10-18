d253e24f12ac84c04de1d27a4345c43d724ca3a6
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-10-18 00:36:10 -0700

    Specify machine when building

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 # Configuration
 SRC_ROOT="${SRC_ROOT:=$(pwd)}"
 POKY_DIR="${POKY_DIR:=$SRC_ROOT/../poky}"
+MACHINE="${MACHINE:=beaglebone-yocto}"
 
 # Build Yocto Poky
 cd $POKY_DIR
@@ -13,7 +14,7 @@ bitbake-layers add-layer $SRC_ROOT
 CONF_FILE=./conf/local.conf
 rm -rf $CONF_FILE
 echo "# Swift for Yocto" >> $CONF_FILE
-echo 'MACHINE="beaglebone-yocto"' >> $CONF_FILE
+echo "MACHINE=\"${MACHINE}\"" >> $CONF_FILE
 echo 'SSTATE_MIRRORS ?= "file://.* http://sstate.yoctoproject.org/all/PATH;downloadfilename=PATH"' >> $CONF_FILE
 
 # build Swift
