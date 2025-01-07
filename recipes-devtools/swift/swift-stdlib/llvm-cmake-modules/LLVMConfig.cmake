b9c2cbe7a0f4cdddaa14e8b2a5231a7cd0cec1b6
Luke Howard <lukeh@padl.com>
2025-01-07 22:29:28 +0000

    snapshot llvm 17

---
+++ recipes-devtools/swift/swift-stdlib/llvm-cmake-modules/LLVMConfig.cmake
@@ -0,0 +1,144 @@
+# This file provides information and services to the final user.
+
+# Compute the installation prefix from this LLVMConfig.cmake file location.
+get_filename_component(LLVM_INSTALL_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
+get_filename_component(LLVM_INSTALL_PREFIX "${LLVM_INSTALL_PREFIX}" PATH)
+get_filename_component(LLVM_INSTALL_PREFIX "${LLVM_INSTALL_PREFIX}" PATH)
+get_filename_component(LLVM_INSTALL_PREFIX "${LLVM_INSTALL_PREFIX}" PATH)
+
+set(LLVM_VERSION_MAJOR 17)
+set(LLVM_VERSION_MINOR 0)
+set(LLVM_VERSION_PATCH 0)
+set(LLVM_VERSION_SUFFIX git)
+set(LLVM_PACKAGE_VERSION 17.0.0git)
+set(LLVM_PACKAGE_BUGREPORT https://bugs.llvm.org/)
+
+set(LLVM_BUILD_TYPE Release)
+
+set(LLVM_USE_CRT_DEBUG )
+set(LLVM_USE_CRT_MINSIZEREL )
+set(LLVM_USE_CRT_RELEASE )
+set(LLVM_USE_CRT_RELWITHDEBINFO )
+
+set(LLVM_USE_SPLIT_DWARF OFF)
+
+set(LLVM_COMMON_DEPENDS )
+
+set(LLVM_AVAILABLE_LIBS )
+
+set(LLVM_DYLIB_COMPONENTS all)
+
+set(LLVM_ALL_TARGETS AArch64;AMDGPU;ARM;AVR;BPF;Hexagon;Lanai;Mips;MSP430;NVPTX;PowerPC;RISCV;Sparc;SystemZ;WebAssembly;X86;XCore)
+
+set(LLVM_TARGETS_TO_BUILD AArch64;AMDGPU;ARM;AVR;BPF;Hexagon;Lanai;Mips;MSP430;NVPTX;PowerPC;RISCV;Sparc;SystemZ;WebAssembly;X86;XCore)
+
+set(LLVM_TARGETS_WITH_JIT X86;PowerPC;AArch64;ARM;Mips;SystemZ)
+
+set_property(GLOBAL PROPERTY LLVMBUILD_LIB_DEPS_LTO )
+
+set(TARGET_TRIPLE "x86_64-unknown-linux-gnu")
+
+set(LLVM_HOST_TRIPLE "x86_64-unknown-linux-gnu")
+
+set(LLVM_ABI_BREAKING_CHECKS WITH_ASSERTS)
+
+set(LLVM_ENABLE_WARNINGS ON)
+
+set(LLVM_ENABLE_EXPENSIVE_CHECKS OFF)
+
+set(LLVM_ENABLE_ASSERTIONS OFF)
+
+set(LLVM_ENABLE_EH OFF)
+
+set(LLVM_ENABLE_RTTI OFF)
+
+set(LLVM_ENABLE_TERMINFO 1)
+
+set(LLVM_ENABLE_THREADS ON)
+
+set(LLVM_ENABLE_UNWIND_TABLES ON)
+
+set(LLVM_ENABLE_ZLIB 1)
+if(LLVM_ENABLE_ZLIB)
+  find_package(ZLIB)
+endif()
+
+set(LLVM_ENABLE_LIBXML2 1)
+if(LLVM_ENABLE_LIBXML2)
+  find_package(LibXml2)
+endif()
+
+set(LLVM_WITH_Z3 )
+
+set(LLVM_ENABLE_DIA_SDK 0)
+
+set(LLVM_NATIVE_ARCH X86)
+
+set(LLVM_ENABLE_PIC ON)
+
+set(LLVM_BUILD_32_BITS OFF)
+
+set(LLVM_ENABLE_NEW_PASS_MANAGER TRUE)
+
+if (NOT "" STREQUAL "")
+  set(LLVM_PTHREAD_LIB "")
+endif()
+
+set(LLVM_ENABLE_PLUGINS ON)
+set(LLVM_EXPORT_SYMBOLS_FOR_PLUGINS OFF)
+set(LLVM_PLUGIN_EXT .so)
+
+set(LLVM_ON_UNIX 1)
+
+set(LLVM_LIBDIR_SUFFIX )
+
+set(LLVM_INCLUDE_DIRS "${LLVM_INSTALL_PREFIX}/include")
+set(LLVM_LIBRARY_DIRS "${LLVM_INSTALL_PREFIX}/lib${LLVM_LIBDIR_SUFFIX}")
+
+set(LLVM_APPEND_VC_REV "ON")
+
+# These variables are duplicated for install tree but they have different
+# values for build tree.  LLVM_INCLUDE_DIRS contains both source
+# and generated include directories while the following variables have
+# them split.
+set(LLVM_INCLUDE_DIR "${LLVM_INSTALL_PREFIX}/include")
+set(LLVM_MAIN_INCLUDE_DIR "${LLVM_INSTALL_PREFIX}/include")
+set(LLVM_LIBRARY_DIR "${LLVM_INSTALL_PREFIX}/lib${LLVM_LIBDIR_SUFFIX}")
+
+set(LLVM_DEFINITIONS "-D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS")
+set(LLVM_CMAKE_DIR "${LLVM_INSTALL_PREFIX}/lib/cmake/llvm")
+set(LLVM_BINARY_DIR "${LLVM_INSTALL_PREFIX}")
+set(LLVM_TOOLS_BINARY_DIR "${LLVM_INSTALL_PREFIX}/bin")
+set(LLVM_TOOLS_INSTALL_DIR "bin")
+set(LLVM_HAVE_OPT_VIEWER_MODULES 0)
+set(LLVM_CONFIGURATION_TYPES )
+set(LLVM_ENABLE_SHARED_LIBS OFF)
+
+set(LLVM_DEFAULT_EXTERNAL_LIT "")
+set(LLVM_LIT_ARGS "")
+
+set(LLVM_HAVE_LIBXAR "")
+
+if(NOT TARGET LLVMSupport)
+  include("${LLVM_CMAKE_DIR}/LLVMExports.cmake")
+  
+endif()
+
+# By creating the following targets here, subprojects that depend on
+# LLVM's tablegen-generated headers can always depend on this target
+# whether building in-tree with LLVM or not.
+if(NOT TARGET intrinsics_gen)
+  add_custom_target(intrinsics_gen)
+endif()
+if(NOT TARGET omp_gen)
+  add_custom_target(omp_gen)
+endif()
+if(NOT TARGET acc_gen)
+  add_custom_target(acc_gen)
+endif()
+if(NOT TARGET RISCVTargetParserTableGen)
+  add_custom_target(RISCVTargetParserTableGen)
+endif()
+
+set_property(GLOBAL PROPERTY LLVM_TARGETS_CONFIGURED On)
+include(${LLVM_CMAKE_DIR}/LLVM-Config.cmake)
