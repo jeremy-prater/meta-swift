57f85f11a2e4038de6331aba235d15d051d36494
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-06-27 19:42:58 +0200

    qemuarm

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 # Configuration
 SRC_ROOT="${SRC_ROOT:=$(pwd)}"
 POKY_DIR="${POKY_DIR:=$SRC_ROOT/../poky}"
-MACHINE="${MACHINE:=beaglebone-yocto}"
+MACHINE="${MACHINE:=qemuarm}"
 
 # Build Yocto Poky
 cd $POKY_DIR
@@ -16,8 +16,12 @@ CONF_FILE=./conf/local.conf
 rm -rf $CONF_FILE
 echo "# Swift for Yocto" >> $CONF_FILE
 echo "MACHINE=\"${MACHINE}\"" >> $CONF_FILE
+echo 'IMAGE_FEATURES += "debug-tweaks"' >> $CONF_FILE
+echo 'IMAGE_INSTALL:append = " swift-hello-world"' >> $CONF_FILE
+
+
 #echo 'SSTATE_MIRRORS ?= "file://.* http://sstate.yoctoproject.org/all/PATH;downloadfilename=PATH"' >> $CONF_FILE
 #echo "USER_CLASSES += \"buildstats buildstats-summary\"" >> $CONF_FILE
 
 # build Swift
-bitbake swift-hello-world
+bitbake core-image-minimal
